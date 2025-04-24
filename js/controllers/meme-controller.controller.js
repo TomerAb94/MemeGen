8e235562b73c1c843efe1c69e3ffdc0e96d1686a
Tomer1994Ab <Abarbanel1994@gmail.com>
2025-04-24 16:54:57 +0300

    Hide gallery when click img

--- js/controllers/meme-controller.controller.js
+++ js/controllers/meme-controller.controller.js
@@ -1,1 +1,24 @@
 'use strict'
+
+var gElCanvas
+var gCtx
+
+function onInitGen(){
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
+    // onResize()
+    // renderMeme()
+    hideSection('.gallery-container')
+}
+
+function renderMeme(){
+
+}
+
+function hideSection(containerName){
+    document.querySelector(containerName).classList.add('hide')
+}
+
+function displaySection(containerName){
+    document.querySelector(containerName).classList.remove('hide')
+}
